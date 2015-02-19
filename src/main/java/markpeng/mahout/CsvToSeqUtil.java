02fed7de6f6111348e40142ceec0175f4f975f7d
guitarmind <markpeng73@gmail.com>
2015-02-19 13:58:20 +0800

    Add HdfsAPITest

---
+++ src/main/java/markpeng/mahout/CsvToSeqUtil.java
@@ -0,0 +1,100 @@
+package markpeng.mahout;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.net.URI;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.Text;
+
+public class CsvToSeqUtil {
+
+	public static void csvToSingleValueSeqFile(String inputFile,
+			String outputFile, String valueColumn) throws Exception {
+		// int NUM_COLUMNS = 1;
+
+		Configuration conf = new Configuration();
+		conf.set("fs.defaultFS", "hdfs://localhost:9000");
+		conf.set("fs.hdfs.impl",
+				org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
+
+		FileSystem fs = FileSystem.get(conf);
+		Path path = new Path(outputFile);
+
+		// FileStatus[] status = fs.listStatus(path);
+		// for (int i = 0; i < status.length; i++) {
+		// BufferedReader br = new BufferedReader(new InputStreamReader(
+		// fs.open(status[i].getPath())));
+		// String line;
+		// line = br.readLine();
+		// while (line != null) {
+		// System.out.println(line);
+		// line = br.readLine();
+		// }
+		// }
+
+		// SequenceFile.Writer writer = new SequenceFile.Writer(fs, conf, path,
+		// Text.class, Text.class);
+		SequenceFile.Writer writer = new SequenceFile.Writer(FileSystem.get(
+				URI.create(inputFile), conf), conf, path, Text.class,
+				Text.class);
+
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(inputFile));
+			String aLine;
+			// get columns of CSV
+			aLine = br.readLine();
+			String[] columns = aLine.split(",");
+			int valueIndex = -1;
+			for (int i = 0; i < columns.length; i++) {
+				if (columns[i].equals(valueColumn)) {
+					valueIndex = i;
+					break;
+				}
+			}
+			int rowIndex = 0;
+			while ((aLine = br.readLine()) != null) {
+				String[] rowData = aLine.split(",");
+				String key = Integer.toString(rowIndex);
+				writer.append(new Text(key), new Text(rowData[valueIndex]));
+
+				rowIndex++;
+			}
+		} finally {
+			if (br != null)
+				br.close();
+			writer.close();
+		}
+
+		// check result file
+		SequenceFile.Reader reader = new SequenceFile.Reader(fs, new Path(
+				outputFile), conf);
+		// SequenceFile.Reader reader = new SequenceFile.Reader(FileSystem.get(
+		// URI.create(outputFile), conf), new Path(outputFile), conf);
+		Text key = new Text();
+		Text value = new Text();
+		while (reader.next(key, value)) {
+			System.out.println(key.toString() + " , " + value.toString());
+		}
+		reader.close();
+	}
+
+	public static void main(String[] args) throws Exception {
+		// String libpath = System.getProperty("java.library.path");
+		// System.out.println("LIBPATH =" + libpath);
+
+		String INPUT_FILE = "hdfs://localhost:9000/user/markpeng/testdata/out_2014-12-08.csv";
+		// String INPUT_FILE = "/user/markpeng/testdata/out_2014-12-08.csv";
+		// String INPUT_FILE =
+		// "/home/markpeng/Share/UitoxSearchLog/RegularCustomer/AW000001/out_2014-12-08.csv";
+		String OUTPUT_FILE = "hdfs://localhost:9000/user/markpeng/testdata/out_2014-12-08.seq";
+		// String OUTPUT_FILE = "/home/markpeng/test/out_2014-12-08.seq";
+		CsvToSeqUtil.csvToSingleValueSeqFile(INPUT_FILE, OUTPUT_FILE, "q");
+	}
+}
---
+++ src/main/java/markpeng/mahout/test/HdfsAPITest.java
@@ -0,0 +1,70 @@
+package markpeng.mahout.test;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.util.ReflectionUtils;
+
+public class HdfsAPITest {
+
+	public void showRemoteFileList(String hdfsPath) throws Exception {
+		Configuration conf = new Configuration();
+		conf.set("fs.defaultFS", "hdfs://172.16.10.16:8020");
+
+		FileSystem fs = FileSystem.get(conf);
+		Path path = new Path(hdfsPath);
+
+		FileStatus[] status = fs.listStatus(path);
+		for (int i = 0; i < status.length; i++) {
+			System.out.println(status[i].getPath());
+		}
+	}
+
+	public void showFileList(String hdfsPath) throws Exception {
+		Configuration conf = new Configuration();
+		conf.set("fs.defaultFS", "hdfs://localhost:9000");
+
+		FileSystem fs = FileSystem.get(conf);
+		Path path = new Path(hdfsPath);
+
+		FileStatus[] status = fs.listStatus(path);
+		for (int i = 0; i < status.length; i++) {
+			System.out.println(status[i].getPath());
+		}
+	}
+
+	public void showFileContent(String hdfsPath) throws Exception {
+		Configuration conf = new Configuration();
+		conf.set("fs.defaultFS", "hdfs://172.16.10.16:9000");
+
+		FileSystem fs = FileSystem.get(conf);
+		Path path = new Path(hdfsPath);
+
+		SequenceFile.Reader reader = new SequenceFile.Reader(conf,
+				SequenceFile.Reader.file(path));
+		// SequenceFile.Reader reader = new SequenceFile.Reader(FileSystem.get(
+		// URI.create(hdfsPath), conf), new Path(outputFile), conf);
+
+		Writable key = (Writable) ReflectionUtils.newInstance(
+				reader.getKeyClass(), conf);
+		Writable value = (Writable) ReflectionUtils.newInstance(
+				reader.getValueClass(), conf);
+		System.out.println("Key: " + key.getClass().getName());
+		System.out.println("Value: " + value.getClass().getName());
+		while (reader.next(key, value)) {
+			System.out.println(key + "  <===>  " + value.toString());
+		}
+		reader.close();
+	}
+
+	public static void main(String[] args) throws Exception {
+		HdfsAPITest test = new HdfsAPITest();
+		// test.showFileList("/user/markpeng/testdata");
+		test.showFileContent("hdfs://localhost:9000/user/markpeng/testoutput");
+		// test.showRemoteFileList("/user/hdfs/markpeng/sparse-text-ik/tokenized-documents/part-m-00000");
+	}
+
+}
