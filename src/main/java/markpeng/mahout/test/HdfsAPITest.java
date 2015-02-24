32185d67ab8b161d0e797aeddf717dcbed4bece1
guitarmind <markpeng73@gmail.com>
2015-02-24 23:51:52 +0800

    Change remote testing port for HDFS API.

--- src/main/java/markpeng/mahout/test/HdfsAPITest.java
+++ src/main/java/markpeng/mahout/test/HdfsAPITest.java
@@ -12,7 +12,7 @@ public class HdfsAPITest {
 
 	public void showRemoteFileList(String hdfsPath) throws Exception {
 		Configuration conf = new Configuration();
-		conf.set("fs.defaultFS", "hdfs://172.16.10.16:8020");
+		conf.set("fs.defaultFS", "hdfs://dev-host18:8020");
 
 		FileSystem fs = FileSystem.get(conf);
 		Path path = new Path(hdfsPath);
@@ -36,35 +36,35 @@ public class HdfsAPITest {
 		}
 	}
 
-	// public void showFileContent(String hdfsPath) throws Exception {
-	// Configuration conf = new Configuration();
-	// conf.set("fs.defaultFS", "hdfs://172.16.10.16:9000");
-	//
-	// FileSystem fs = FileSystem.get(conf);
-	// Path path = new Path(hdfsPath);
-	//
-	// SequenceFile.Reader reader = new SequenceFile.Reader(conf,
-	// SequenceFile.Reader.file(path));
-	// // SequenceFile.Reader reader = new SequenceFile.Reader(FileSystem.get(
-	// // URI.create(hdfsPath), conf), new Path(outputFile), conf);
-	//
-	// Writable key = (Writable) ReflectionUtils.newInstance(
-	// reader.getKeyClass(), conf);
-	// Writable value = (Writable) ReflectionUtils.newInstance(
-	// reader.getValueClass(), conf);
-	// System.out.println("Key: " + key.getClass().getName());
-	// System.out.println("Value: " + value.getClass().getName());
-	// while (reader.next(key, value)) {
-	// System.out.println(key + "  <===>  " + value.toString());
-	// }
-	// reader.close();
-	// }
+	public void showFileContent(String hdfsPath) throws Exception {
+		Configuration conf = new Configuration();
+		conf.set("fs.defaultFS", "hdfs://172.16.10.16:9000");
+
+		FileSystem fs = FileSystem.get(conf);
+		Path path = new Path(hdfsPath);
+
+		SequenceFile.Reader reader = new SequenceFile.Reader(conf,
+				SequenceFile.Reader.file(path));
+		// SequenceFile.Reader reader = new SequenceFile.Reader(FileSystem.get(
+		// URI.create(hdfsPath), conf), new Path(outputFile), conf);
+
+		Writable key = (Writable) ReflectionUtils.newInstance(
+				reader.getKeyClass(), conf);
+		Writable value = (Writable) ReflectionUtils.newInstance(
+				reader.getValueClass(), conf);
+		System.out.println("Key: " + key.getClass().getName());
+		System.out.println("Value: " + value.getClass().getName());
+		while (reader.next(key, value)) {
+			System.out.println(key + "  <===>  " + value.toString());
+		}
+		reader.close();
+	}
 
 	public static void main(String[] args) throws Exception {
 		HdfsAPITest test = new HdfsAPITest();
 		// test.showFileList("/user/markpeng/testdata");
 		// test.showFileContent("hdfs://localhost:9000/user/markpeng/testoutput");
-		// test.showRemoteFileList("/user/hdfs/markpeng/sparse-text-ik/tokenized-documents/part-m-00000");
+		test.showRemoteFileList("/user/hdfs/markpeng/sparse-text-ik/tokenized-documents/part-m-00000");
 	}
 
 }
