594c47ae88d2d95e6ad4c544465bdae312cfe2d8
guitarmind <markpeng73@gmail.com>
2015-04-25 17:24:30 +0800

    Completed SentimentFeatureGenerator.

--- src/main/java/markpeng/kaggle/tae/SentimentFeatureGenerator.java
+++ src/main/java/markpeng/kaggle/tae/SentimentFeatureGenerator.java
@@ -43,83 +43,383 @@ public class SentimentFeatureGenerator {
 		return features;
 	}
 
-	public void generate(String trainFile, String testFile, String outputCsv,
-			String... featureFiles) throws Exception {
-		List<String> features = readFeature(featureFiles);
+	public void generate(String trainFile, String testFile, String outputTrain,
+			String outputTest, String... featureFiles) throws Exception {
+		List<String> sentiments = readFeature(featureFiles);
 
 		StringBuffer resultStr = new StringBuffer();
 
-		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(
-				new FileOutputStream(outputCsv, false), "UTF-8"));
+		BufferedReader trainIn = new BufferedReader(new InputStreamReader(
+				new FileInputStream(trainFile), "UTF-8"));
+
+		BufferedReader testIn = new BufferedReader(new InputStreamReader(
+				new FileInputStream(testFile), "UTF-8"));
+
+		BufferedWriter trainOut = new BufferedWriter(new OutputStreamWriter(
+				new FileOutputStream(outputTrain, false), "UTF-8"));
+
+		BufferedWriter testOut = new BufferedWriter(new OutputStreamWriter(
+				new FileOutputStream(outputTest, false), "UTF-8"));
+
+		// popular sentiments
+		List<String> popSentiments = sentimentFeatureExtraction(trainFile,
+				testFile, sentiments);
+
+		// prefix with ':'
+		List<String> prefixFeatures = prefixFeatureExtraction(trainFile,
+				testFile);
+
+		// create headers
+		int i = 0;
+		for (String senti : popSentiments) {
+			resultStr.append("Senti_" + (i + 1) + ",");
+			i++;
+		}
+		int p = 0;
+		for (String prefix : prefixFeatures) {
+			resultStr.append("Prefix_" + (p + 1) + ",");
+			p++;
+		}
+		resultStr.append("Five_Who,");
+		resultStr.append("Five_What,");
+		resultStr.append("Five_Where,");
+		resultStr.append("Five_When,");
+		resultStr.append("Five_How,");
+		resultStr.append("qmark,");
+		resultStr.append("exmark,");
+		resultStr.append("please,");
+		resultStr.append("yearNumWithSemi,");
+		resultStr.append("yearNum,");
+		resultStr.append("recap,");
+		resultStr.append("report");
+		resultStr.append(newLine);
+
 		try {
-			int validN = 0;
 
-			List<String> prefixFeatures = prefixFeatureExtraction(trainFile,
-					testFile);
+			String aLine = null;
+			// skip header
+			trainIn.readLine();
+			while ((aLine = trainIn.readLine()) != null) {
+				int[] rowResult = new int[popSentiments.size()
+						+ prefixFeatures.size() + 12];
 
-			// how, what, when, where, who
+				String tmp = aLine.toLowerCase().trim();
+				String[] tokens = tmp.split(",");
+				String headline = tokens[3].replace("\"", "").trim();
+				String abst = tokens[5].replace("\"", "").trim();
 
-			// question mark
+				int sIndex = 0;
+				for (String s : popSentiments) {
+					if (headline.contains(s) || abst.contains(s)) {
+						rowResult[sIndex] = 1;
+						System.out.println(s + " ==> " + headline + "," + abst);
+					}
 
-			// ! mark
+					sIndex++;
+				}
 
-			// please
+				// System.out.println(headline);
 
-			// contains a year number with a ':'
+				int hlength = headline.length();
+				if (headline.contains(":")
+						&& headline.indexOf(":") <= (hlength / 2)) {
+					String newFeature = headline.substring(0,
+							headline.indexOf(":")).trim();
 
-			// contains a year number
+					int pIndex = popSentiments.size();
+					for (String t : prefixFeatures) {
+						if (newFeature.equals(t))
+							rowResult[pIndex] = 1;
 
-			// 'Daily Report:'
+						pIndex++;
+					}
+				}
 
-			// 'Morning Agenda:'
+				int otherStart = popSentiments.size() + prefixFeatures.size();
+				// who, what, where, when, how
+				if (headline.contains("who"))
+					rowResult[otherStart] = 1;
+				if (headline.contains("what"))
+					rowResult[otherStart + 1] = 1;
+				if (headline.contains("where"))
+					rowResult[otherStart + 2] = 1;
+				if (headline.contains("when"))
+					rowResult[otherStart + 3] = 1;
+				if (headline.contains("how"))
+					rowResult[otherStart + 4] = 1;
 
-			// 'In Performance:'
+				// question mark
+				if (headline.contains("?"))
+					rowResult[otherStart + 5] = 1;
 
-			// 'Q. and A.:'
+				// ! mark
+				if (headline.contains("!"))
+					rowResult[otherStart + 6] = 1;
 
-			// 'Pictures of the Day:'
+				// please
+				if (headline.contains("please"))
+					rowResult[otherStart + 7] = 1;
 
-			// 'Walkabout:'
+				// contains a year number with a ':'
+				if (headline.matches("[0-9]{4,4}:.*")) {
+					rowResult[otherStart + 8] = 1;
+					// System.out.println(headline + " ==> yearNumWithSemi");
+				}
 
-			// 'New York Fashion Week:'
+				// contains a year number
+				if (headline.matches("[0-9]{4,4}.*")) {
+					rowResult[otherStart + 9] = 1;
+					// System.out.println(headline + " ==> yearNum");
+				}
 
-			// 'New York Today:'
+				// Recap
+				if (headline.contains("recap"))
+					rowResult[otherStart + 10] = 1;
 
-			// ''The Newsroom' Recap:' => popular
+				// Report
+				if (headline.contains("report"))
+					rowResult[otherStart + 11] = 1;
 
-			// Recap
+				// append output row
+				int vIndex = 0;
+				for (Integer v : rowResult) {
+					if (vIndex < rowResult.length - 1)
+						resultStr.append(v + ",");
+					else
+						resultStr.append(v + newLine);
+					vIndex++;
+				}
 
-			// Report
+				if (resultStr.length() >= BUFFER_LENGTH) {
+					trainOut.write(resultStr.toString());
+					trainOut.flush();
+					resultStr.setLength(0);
+				}
 
-			// 'Ask Well:'
-
-			System.out.println("Total # of sentiment features:" + validN);
+			} // end of train file loop
 
 		} finally {
-			out.write(resultStr.toString());
-			out.flush();
-			out.close();
+			trainIn.close();
+
+			trainOut.write(resultStr.toString());
+			trainOut.flush();
+			trainOut.close();
+			resultStr.setLength(0);
+		}
+
+		// ---------------------------------------------------------------------------------------------------------------
+		// test dataset
+
+		try {
+			resultStr.setLength(0);
+			// create headers
+			i = 0;
+			for (String senti : popSentiments) {
+				resultStr.append("Senti_" + (i + 1) + ",");
+				i++;
+			}
+			p = 0;
+			for (String prefix : prefixFeatures) {
+				resultStr.append("Prefix_" + (p + 1) + ",");
+				p++;
+			}
+			resultStr.append("Five_Who,");
+			resultStr.append("Five_What,");
+			resultStr.append("Five_Where,");
+			resultStr.append("Five_When,");
+			resultStr.append("Five_How,");
+			resultStr.append("qmark,");
+			resultStr.append("exmark,");
+			resultStr.append("please,");
+			resultStr.append("yearNumWithSemi,");
+			resultStr.append("yearNum,");
+			resultStr.append("recap,");
+			resultStr.append("report");
+			resultStr.append(newLine);
+
+			String aLine = null;
+			// skip header
+			testIn.readLine();
+			while ((aLine = testIn.readLine()) != null) {
+				int[] rowResult = new int[popSentiments.size()
+						+ prefixFeatures.size() + 12];
+
+				String tmp = aLine.toLowerCase().trim();
+				String[] tokens = tmp.split(",");
+				String headline = tokens[3].replace("\"", "").trim();
+				String abst = tokens[5].replace("\"", "").trim();
+
+				int sIndex = 0;
+				for (String s : popSentiments) {
+					if (headline.contains(s) || abst.contains(s)) {
+						rowResult[sIndex] = 1;
+						System.out.println(s + " ==> " + headline + "," + abst);
+					}
+
+					sIndex++;
+				}
+
+				// System.out.println(headline);
+				int hlength = headline.length();
+				if (headline.contains(":")
+						&& headline.indexOf(":") <= (hlength / 2)) {
+					String newFeature = headline.substring(0,
+							headline.indexOf(":")).trim();
+
+					int pIndex = popSentiments.size();
+					for (String t : prefixFeatures) {
+						if (newFeature.equals(t))
+							rowResult[pIndex] = 1;
+
+						pIndex++;
+					}
+				}
+
+				int otherStart = popSentiments.size() + prefixFeatures.size();
+				// who, what, where, when, how
+				if (headline.contains("who"))
+					rowResult[otherStart] = 1;
+				if (headline.contains("what"))
+					rowResult[otherStart + 1] = 1;
+				if (headline.contains("where"))
+					rowResult[otherStart + 2] = 1;
+				if (headline.contains("when"))
+					rowResult[otherStart + 3] = 1;
+				if (headline.contains("how"))
+					rowResult[otherStart + 4] = 1;
+
+				// question mark
+				if (headline.contains("?"))
+					rowResult[otherStart + 5] = 1;
+
+				// ! mark
+				if (headline.contains("!"))
+					rowResult[otherStart + 6] = 1;
+
+				// please
+				if (headline.contains("please"))
+					rowResult[otherStart + 7] = 1;
+
+				// contains a year number with a ':'
+				if (headline.matches("[0-9]{4,4}:.*")) {
+					rowResult[otherStart + 8] = 1;
+					System.out.println(headline + " ==> yearNumWithSemi");
+				}
+
+				// contains a year number
+				if (headline.matches("[0-9]{4,4}.*")) {
+					rowResult[otherStart + 9] = 1;
+					System.out.println(headline + " ==> yearNum");
+				}
+
+				// Recap
+				if (headline.contains("recap"))
+					rowResult[otherStart + 10] = 1;
+
+				// Report
+				if (headline.contains("report"))
+					rowResult[otherStart + 11] = 1;
+
+				// append output row
+				int vIndex = 0;
+				for (Integer v : rowResult) {
+					if (vIndex < rowResult.length - 1)
+						resultStr.append(v + ",");
+					else
+						resultStr.append(v + newLine);
+					vIndex++;
+				}
+
+				if (resultStr.length() >= BUFFER_LENGTH) {
+					testOut.write(resultStr.toString());
+					testOut.flush();
+					resultStr.setLength(0);
+				}
+
+			} // end of train file loop
+
+		} finally {
+			testIn.close();
+
+			testOut.write(resultStr.toString());
+			testOut.flush();
+			testOut.close();
 			resultStr.setLength(0);
 		}
 	}
 
-	public static <K, V extends Comparable<? super V>> SortedSet<Map.Entry<K, V>> entriesSortedByValues(
-			Map<K, V> map) {
-		SortedSet<Map.Entry<K, V>> sortedEntries = new TreeSet<Map.Entry<K, V>>(
-				new Comparator<Map.Entry<K, V>>() {
-					@Override
-					public int compare(Map.Entry<K, V> e1, Map.Entry<K, V> e2) {
-						int res = e1.getValue().compareTo(e2.getValue());
-						if (res > 0)
-							return -1;
-						if (res < 0)
-							return 1;
+	public List<String> sentimentFeatureExtraction(String trainFile,
+			String testFile, List<String> sentiments) throws Exception {
+		List<String> newFeatures = new ArrayList<String>();
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				new FileInputStream(trainFile), "UTF-8"));
+
+		BufferedReader testIn = new BufferedReader(new InputStreamReader(
+				new FileInputStream(testFile), "UTF-8"));
+
+		try {
+			String aLine = null;
+			// skip header
+			in.readLine();
+			Hashtable<String, Integer> sentiTable = new Hashtable<String, Integer>();
+			while ((aLine = in.readLine()) != null) {
+				String tmp = aLine.toLowerCase().trim();
+				String[] tokens = tmp.split(",");
+				String headline = tokens[3].replace("\"", "").trim();
+				String abst = tokens[5].replace("\"", "").trim();
+				String popular = tokens[tokens.length - 2];
+
+				for (String s : sentiments) {
+					if ((headline.contains(s) || abst.contains(s))
+							&& popular.equals("1")) {
+						if (!sentiTable.containsKey(s))
+							sentiTable.put(s, 1);
 						else
-							return res;
+							sentiTable.put(s, sentiTable.get(s) + 1);
 					}
-				});
-		sortedEntries.addAll(map.entrySet());
-		return sortedEntries;
+				}
+			}
+
+			int sentiCount = 0;
+			for (String t : sentiTable.keySet()) {
+				int freq = sentiTable.get(t);
+				System.out.println(t + " ==> " + freq);
+				sentiCount++;
+			}
+
+			System.out.println("Total detected sentiment features: "
+					+ sentiCount);
+
+			// make sure that test csv contains them
+			aLine = null;
+			// skip header
+			in.readLine();
+			while ((aLine = testIn.readLine()) != null) {
+				String tmp = aLine.toLowerCase().trim();
+				String[] tokens = tmp.split(",");
+				String headline = tokens[3].replace("\"", "").trim();
+				String abst = tokens[5].replace("\"", "").trim();
+
+				for (String t : sentiTable.keySet()) {
+					if (headline.contains(t) || abst.contains(t)) {
+						if (!newFeatures.contains(t))
+							newFeatures.add(t);
+					}
+				}
+
+			}
+
+			for (String t : newFeatures) {
+				System.out.println(t);
+			}
+			System.out.println("Final feature count: " + newFeatures.size());
+		} finally {
+			in.close();
+			testIn.close();
+		}
+
+		return newFeatures;
 	}
 
 	public List<String> prefixFeatureExtraction(String trainFile,
@@ -209,24 +509,35 @@ public class SentimentFeatureGenerator {
 	}
 
 	public static void main(String[] args) throws Exception {
-		args = new String[4];
+		args = new String[5];
 		args[0] = "/home/markpeng/Share/Kaggle/The Analytics Edge Competition/NYTimesBlogTrain.csv";
 		args[1] = "/home/markpeng/Share/Kaggle/The Analytics Edge Competition/NYTimesBlogTest.csv";
 		args[2] = "/home/markpeng/Share/Kaggle/The Analytics Edge Competition/sentiment/AFINN-111.txt";
-		args[3] = "/home/markpeng/Share/Kaggle/The Analytics Edge Competition/sentimentTrainTest.csv";
+		// args[3] =
+		// "/home/markpeng/Share/Kaggle/The Analytics Edge Competition/specialTrain.csv";
+		// args[4] =
+		// "/home/markpeng/Share/Kaggle/The Analytics Edge Competition/specialTest.csv";
+		args[3] = "/home/markpeng/Share/Kaggle/The Analytics Edge Competition/sentimentTrain.csv";
+		args[4] = "/home/markpeng/Share/Kaggle/The Analytics Edge Competition/sentimentTest.csv";
 
-		if (args.length < 4) {
+		if (args.length < 5) {
 			System.out
-					.println("Arguments: [train.csv] [test.csv] [featureFiles] [output txt]");
+					.println("Arguments: [train.csv] [test.csv] [featureFiles] [output train] [output test]");
 			return;
 		}
 		String trainFile = args[0];
 		String testFile = args[1];
 		String[] featureFiles = args[2].split("\\|");
-		String outputCsv = args[3];
+		String outputTrain = args[3];
+		String outputTest = args[4];
 		SentimentFeatureGenerator worker = new SentimentFeatureGenerator();
-		// worker.generate(trainFile, testFile, outputCsv, featureFiles);
-		worker.prefixFeatureExtraction(trainFile, testFile);
+		worker.generate(trainFile, testFile, outputTrain, outputTest,
+				featureFiles);
+		// worker.prefixFeatureExtraction(trainFile, testFile);
+		// List<String> features = worker.readFeature(featureFiles);
+		// worker.sentimentFeatureExtraction(trainFile, testFile, features);
 
+		// System.out.println("1914: russians dominate in east poland"
+		// .matches("[0-9]{4,4}:.*"));
 	}
 }
