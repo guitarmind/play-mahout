b7831395e4762363c7ef27c0bd35dc60a073c8d3
guitarmind <markpeng73@gmail.com>
2015-10-03 00:57:01 +0800

    Extract tag counts.

--- src/main/java/markpeng/kaggle/smr/FeatureExtractor.java
+++ src/main/java/markpeng/kaggle/smr/FeatureExtractor.java
@@ -8,10 +8,22 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
+import java.util.Set;
 
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.core.LowerCaseFilter;
+import org.apache.lucene.analysis.core.StopFilter;
+import org.apache.lucene.analysis.en.PorterStemFilter;
+import org.apache.lucene.analysis.miscellaneous.WordDelimiterFilter;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.standard.StandardTokenizer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.util.CharArraySet;
+import org.apache.lucene.util.Version;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
@@ -28,12 +40,12 @@ public class FeatureExtractor implements Runnable {
 
 	private String folderId = null;
 
-	private final String[] TAGS = { "html", "head", "meta", "title", "body",
-			"p", "a", "image", "base", "link", "script", "style", "div", "ul",
-			"li", "span", "i", "nav", "button", "form", "iframe", "h1", "h2",
-			"h3", "h4", "h5", "br", "input", "b", "script", "em", "table",
-			"tr", "td", "hr", "svg", "symbol", "path", "g", "use", "label",
-			"section", "noscript", "article", "footer" };
+	private final String[] TAGS = { "meta", "title", "body", "p", "a", "image",
+			"base", "link", "script", "style", "div", "ul", "li", "span", "i",
+			"nav", "button", "form", "iframe", "h1", "h2", "h3", "h4", "h5",
+			"br", "input", "b", "em", "table", "tr", "td", "hr", "svg",
+			"symbol", "path", "g", "use", "label", "section", "noscript",
+			"article", "footer" };
 
 	public FeatureExtractor(String htmlFolderPath,
 			Hashtable<String, String> trainFileList, List<String> testFileList,
@@ -99,11 +111,33 @@ public class FeatureExtractor implements Runnable {
 	@Override
 	public void run() {
 		try {
-			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(
-					new FileOutputStream(outputTrain, false), "UTF-8"));
+			BufferedWriter out = null;
 			StringBuffer resultStr = new StringBuffer();
 
 			try {
+				if (folderId.equals("5")) {
+					out = new BufferedWriter(new OutputStreamWriter(
+							new FileOutputStream(outputTest, false), "UTF-8"));
+				} else {
+					out = new BufferedWriter(new OutputStreamWriter(
+							new FileOutputStream(outputTrain, false), "UTF-8"));
+				}
+
+				if (folderId.equals("0") || folderId.equals("5")) {
+					// create headers
+					resultStr.append("\"file\",");
+					int tindex = 0;
+					for (String t : TAGS) {
+						if (tindex != TAGS.length - 1)
+							resultStr.append("\"" + t + "_count\",");
+						else
+							resultStr.append("\"" + t + "_count\"");
+
+						tindex++;
+					}
+					resultStr.append(newLine);
+				}
+
 				File checker = new File(htmlFolderPath + "/" + folderId);
 				if (checker.exists()) {
 					System.out.println("Folder " + folderId + ": "
@@ -119,6 +153,10 @@ public class FeatureExtractor implements Runnable {
 
 					for (String file : files) {
 
+						String fileName = file
+								.substring(file.lastIndexOf("/") + 1);
+						resultStr.append("\"" + fileName + "\",");
+
 						BufferedReader in = new BufferedReader(
 								new InputStreamReader(
 										new FileInputStream(file), "UTF-8"));
@@ -132,10 +170,29 @@ public class FeatureExtractor implements Runnable {
 
 							Document doc = Jsoup.parse(htmlStr.toString());
 							// get all raw text
-							String title = doc.title();
-							System.out.println(title);
-							String allText = doc.text();
-							System.out.println(allText.length());
+							// String title = doc.title();
+							// System.out.println(title);
+							// String allText = doc.body().text().trim();
+							// System.out.println(allText.length());
+							//
+							// String processedTitle =
+							// processTextByLucene(title);
+							// String processedText =
+							// processTextByLucene(allText);
+
+							// get tag counts
+							int tindex = 0;
+							for (String t : TAGS) {
+								int count = doc.select(t).size();
+								if (tindex != TAGS.length - 1)
+									resultStr.append(count + ",");
+								else
+									resultStr.append(count);
+
+								tindex++;
+							}
+							resultStr.append(newLine);
+
 						} finally {
 							in.close();
 						}
@@ -168,6 +225,113 @@ public class FeatureExtractor implements Runnable {
 
 	}
 
+	public String processTextByLucene(String text) throws IOException {
+		String result = text;
+
+		StringBuffer postText = new StringBuffer();
+		// Set stopWords = new StandardAnalyzer(Version.LUCENE_46)
+		// .getStopwordSet();
+		TokenStream ts = new StandardTokenizer(Version.LUCENE_46,
+				new StringReader(text));
+		// ts = new StopFilter(Version.LUCENE_46, ts, (CharArraySet) stopWords);
+		int flags = WordDelimiterFilter.SPLIT_ON_NUMERICS
+				| WordDelimiterFilter.SPLIT_ON_CASE_CHANGE
+				| WordDelimiterFilter.GENERATE_NUMBER_PARTS
+				| WordDelimiterFilter.GENERATE_WORD_PARTS;
+		ts = new WordDelimiterFilter(ts, flags, null);
+		ts = new LowerCaseFilter(Version.LUCENE_46, ts);
+		ts = new PorterStemFilter(ts);
+		// ts = new DictionaryCompoundWordTokenFilter(Version.LUCENE_46, ts,
+		// new CharArraySet(Version.LUCENE_46, dictionary, true), 6, 4,
+		// 10, false);
+
+		// less aggressive, better than PorterStemFilter!
+		// ts = new KStemFilter(ts);
+
+		try {
+			CharTermAttribute termAtt = ts
+					.addAttribute(CharTermAttribute.class);
+			ts.reset();
+			// int wordCount = 0;
+			while (ts.incrementToken()) {
+				if (termAtt.length() > 0) {
+					String word = termAtt.toString();
+
+					postText.append(word + " ");
+					// System.out.println(word);
+
+					// wordCount++;
+				}
+			}
+
+			String finalText = postText.toString().trim().replace("'", "");
+			if (finalText.length() > 1)
+				result = finalText;
+
+		} finally {
+			// Fixed error : close ts:TokenStream
+			ts.end();
+			ts.close();
+		}
+
+		return result;
+	}
+
+	public boolean isAllEnglish(String text) {
+		boolean result = true;
+
+		String[] tokens = text.split("\\s");
+
+		for (String token : tokens) {
+			for (int i = 0; i < token.length(); i++) {
+				char c = token.charAt(i);
+				if (!Character.isAlphabetic(c) && c != '-') {
+					result = false;
+					break;
+				}
+			}
+		}
+
+		return result;
+	}
+
+	public boolean isAllDigits(String text) {
+		boolean result = true;
+
+		String[] tokens = text.split("\\s");
+
+		for (String token : tokens) {
+			for (int i = 0; i < token.length(); i++) {
+				char c = token.charAt(i);
+				if (!Character.isDigit(c)) {
+					result = false;
+					break;
+				}
+			}
+		}
+
+		return result;
+	}
+
+	public boolean isAllEnglishAndDigits(String text) {
+		boolean result = true;
+
+		String[] tokens = text.split("\\s");
+
+		for (String token : tokens) {
+			for (int i = 0; i < token.length(); i++) {
+				char c = token.charAt(i);
+				if (!Character.isAlphabetic(c) && c != '-'
+						&& !Character.isDigit(c)) {
+					result = false;
+					break;
+				}
+			}
+		}
+
+		return result;
+	}
+
 	public static void main(String[] args) throws Exception {
 		String htmlFolderPath = args[0];
 		String trainFileListPath = args[1];
@@ -184,15 +348,16 @@ public class FeatureExtractor implements Runnable {
 			FeatureExtractor worker = new FeatureExtractor(htmlFolderPath,
 					trainFileList, testFileList, outputTrain, outputTest,
 					Integer.toString(i));
-			threads[i] = new Thread(worker);
-			threads[i].start();
+			// threads[i] = new Thread(worker);
+			// threads[i].start();
+			worker.run();
 
 			System.out.println("Running thread for folder " + i + " ...");
 			Thread.sleep(2000);
 		}
 
-		for (Thread t : threads)
-			t.join();
+		// for (Thread t : threads)
+		// t.join();
 	}
 
 }
